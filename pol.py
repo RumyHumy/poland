4fd89af8de49711737eaa4d9d4f496737c38ffd0
Ramil Khsanshin <khasanshin.ramil@list.ru>
2025-03-24 15:34:41 +0300

    Writen convertation + removed '(' from operators

--- pol.py
+++ pol.py
@@ -2,47 +2,59 @@
 
 import sys
 
-opdic = { "+": 1, "-": 1, "*": 2, "/": 2, "^": 3, "(": -1, ")": -1 }
-
-def opord(c):
-    return opdic.get(c)
+opdic = { "+": 1, "-": 1, "*": 2, "/": 2, "^": 3 }
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Not enough arguments.")
         exit(1)
 
-    # Tokenize
+    # T O K E N I Z E
+
     exp = sys.argv[1]
     infix = ""
     for c in exp:
-        if opord(c):
+        if opdic.get(c) or c == "(" or c == ")":
             infix += " "+c+" "
         else:
             infix += c
     infix = [t for t in infix.split() if len(t) > 0]
     print(f"Infix: {' '.join(infix)}")
-    
-    # Rewrite
-    #print("Rewriting in postfix: ")
-    #stack = []
-    #postfix = []
-    #for t in infix:
-    #    print(f"  token/stack: '{t}': {', '.join(stack)}")
-    #    if t == "(":
-    #        stack.append(t)
-    #    elif t == ")":
-    #        while len(stack) and stack[-1] != "(":
-    #            postfix.append(stack.pop())
-    #        stack.pop()
-    #    elif opord(t[0]):
-    #        while len(stack) and opord(t[0]) <= opord(stack[-1][0]):
-    #            postfix.append(stack.pop())
-    #        stack.append(t)
-    #    else:
-    #        postfix.append(t)
 
-    #while len(stack):
-    #    postfix.append(stack.pop())
+    for i in range(len(infix)-1):
+        if opdic.get(infix[i]) != None and opdic.get(infix[i+1]) != None:
+            print("Invalid expression: two operators in a row")
+            exit(1)
 
-    #print(f"Postfix: {' '.join(postfix)}")
+    # P O S T F I X
+
+    print("Rewriting in postfix: ")
+    print("token/stack")
+    stack = []
+    postfix = []
+    for t in infix:
+        print(f"  '{t}' | '{''.join(stack)}' ", end="")
+        if t == "(":
+            stack.append(t)
+        elif t == ")":
+            while len(stack) and stack[-1] != "(":
+                postfix.append(stack.pop())
+            if not len(stack):
+                print("\nInvalid expression: Excess ')'")
+                exit(1)
+            stack.pop()
+        elif opdic.get(t[0]):
+            while len(stack) and stack[-1] != "(" and opdic.get(t[0]) <= opdic.get(stack[-1][0]):
+                postfix.append(stack.pop())
+            stack.append(t)
+        else:
+            postfix.append(t)
+        print(f"-> '{''.join(stack)}'")
+
+    while len(stack):
+        if stack[-1] == '(':
+            print("Invalid expression: Excess '('")
+            exit(1)
+        postfix.append(stack.pop())
+
+    print(f"Postfix: {' '.join(postfix)}")
