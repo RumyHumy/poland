e38bc504ad54bdd4ba0c27e905931bd24bd43202
Ramil Khsanshin <khasanshin.ramil@list.ru>
2025-03-24 22:11:45 +0300

    WIP: infix, prefix, postfix without ()

--- totree.py
+++ totree.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     for c in sys.argv[1]:
         infix += f" {c} " if opdic.get(c) or c in "()" else c
     infix = [t for t in infix.split() if len(t)]
-    print(infix)
 
     # D U P L I C A T I O N   C H E C K
 
@@ -27,9 +26,58 @@ if __name__ == "__main__":
     
     # T R E E
 
-    #class TreeNode:
-    #    v = None
-    #    l, r = None, None
+    class ExpNode:
+        def __init__(self, value, left, right):
+            self.value = value
+            self.left = left
+            self.right = right
 
-    #def FillRec(toks):
-        
+    stack = [] # Node stack
+    ops = [] # Operand stack
+
+    def AddOp(stack, ops):
+        right = stack.pop()
+        left = stack.pop()
+        node = ExpNode(ops.pop(), left, right)
+        stack.append(node)
+
+    for t in infix:
+        if opdic.get(t):
+            while ops and opdic.get(t) <= opdic.get(ops[-1]):
+                AddOp(stack, ops)
+            ops.append(t)
+        else:
+            stack.append(ExpNode(t, None, None))
+    print()
+    while ops:
+        AddOp(stack, ops)
+
+    # P R I N T
+
+    def PrintInfix(node):
+        if node:
+            PrintInfix(node.left)
+            print(node.value, end=' ')
+            PrintInfix(node.right)
+
+    def PrintPrefix(node):
+        if node:
+            print(node.value, end=' ')
+            PrintInfix(node.left)
+            PrintInfix(node.right)
+
+    def PrintPostfix(node):
+        if node:
+            PrintInfix(node.left)
+            PrintInfix(node.right)
+            print(node.value, end=' ')
+
+    print("INFIX: ")
+    PrintInfix(stack[0])
+    print()
+    print("PREFIX: ")
+    PrintPrefix(stack[0])
+    print()
+    print("POSTFIX: ")
+    PrintPostfix(stack[0])
+    print()
